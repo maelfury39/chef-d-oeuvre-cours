34e1813139cf5d03af1d4d4f766e0e3de8d6a606
Lucas <135503658+lucas-picard@users.noreply.github.com>
2026-04-08 20:16:12 +0200

    Add files via upload

---
+++ Chef_Oeuvre/js/script.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector(".button");
+const info = document.querySelector("p");
+const userLogin = document.querySelector(".input1");
+const userPassword = document.querySelector(".input2");
+
+
+
+btn.addEventListener("click", updateButton);
+
+
+function updateButton() {
+  if (userLogin.value.trim() === "ProB26" && userPassword.value.trim() === "CIELAR2026") {
+    window.location.href = "../html/main.html";
+
+  } else {
+    
+    info.textContent = "Acces refusé.";
+  }
+}
